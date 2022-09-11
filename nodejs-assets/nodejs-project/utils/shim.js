050f3cdcfd63fa8f27ef05dbbac9a7c0ba9b4b98
hritikutekar <hritikdrocks@gmail.com>
2022-09-11 23:29:41 +0530

    fix: polyfill String.prototype.normalize

--- nodejs-assets/nodejs-project/utils/shim.js
+++ nodejs-assets/nodejs-project/utils/shim.js
@@ -1,5 +1,19 @@
+const unorm = require('unorm');
 const fetch = require('node-fetch');
 const AbortController = require('./AbortController');
 
 global.fetch = fetch;
 global.AbortController = AbortController;
+// "NFC", "NFD", "NFKC", or "NFKD"
+global.String.prototype.normalize = function (type) {
+  switch (type) {
+    case 'NFC':
+      return unorm.nfc(this);
+    case 'NFD':
+      return unorm.nfd(this);
+    case 'NFKC':
+      return unorm.nfkc(this);
+    case 'NFKD':
+      return unorm.nfkd(this);
+  }
+};
